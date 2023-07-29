7cbf2450454c96f76e357ae8230aaae3d31acf53
Rory Dudley <rory.dudley@gmail.com>
2023-07-28 22:56:04 -0600

    v1.0.1
    - Added 'parse' method to top-level module, like how the README
      describes. Method can take a String, or a StringIO.
    - Added tests for the top-level 'parse' method.

--- lib/gemtext.rb
+++ lib/gemtext.rb
@@ -1,6 +1,16 @@
+# frozen_string_literal: true
+
 require 'gemtext/version'
 require 'gemtext/parser'
+require 'stringio'
 
 module Gemtext
   class Error < StandardError; end
+
+  def self.parse(data)
+    io = data
+    io = StringIO.new(data) if data.instance_of? String
+    parser = Parser.new(io)
+    parser.parse
+  end
 end
